ac85de3667c08ecf391b90f2cced9e4c3953cb4e
dlgksqls <horainlee87@gmail.com>
2024-12-28 18:49:25 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -10,4 +10,9 @@ public class IndexController {
     public String index(){
         return "index";
     }
+
+    @GetMapping("/loginPage")
+    public String login(){
+        return "loginPage";
+    }
 }
--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -15,35 +15,27 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    @Bean // 사용자 정의 보안
+    @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
         http
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
-                .formLogin(Customizer.withDefaults());
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+
+                .rememberMe(rememberMe -> rememberMe
+                        .alwaysRemember(true)  // "기억하기" 매개변수가 설정되지 않았을 때에도 쿠키가 항상 생성되어야 함
+                        .tokenValiditySeconds(3600)  // 토큰이 유효한 시간(초 단위)
+                        .userDetailsService(userDetailsService())  // UserDetails를 조회하기 위해 사용되는 UserDetailsService 지정
+                        .rememberMeParameter("remember")  // 로그인 시 사용자를 기억하기 위해 사용되는 HTTP 매개변수
+                        .rememberMeCookieName("remember")  // 기억하기 인증을 위한 토큰을 저장하는 쿠키 이름
+                        .key("security")); // 기억하기 기능을 사용하기 위한 키
 
         return http.build();
     }
 
     @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user2 = User
-                .withUsername("user2")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-
-        UserDetails user3 = User
-                .withUsername("user3")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-
-        UserDetails user4 = User
-                .withUsername("user4")
-                .password("{noop}1111")
-                .roles("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(user2, user3, user4);
+        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        return  new InMemoryUserDetailsManager(user);
     }
 }
