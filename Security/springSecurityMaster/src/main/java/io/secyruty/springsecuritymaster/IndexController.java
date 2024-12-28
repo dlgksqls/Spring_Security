8949a263f5356e923fcd970b96751b9ef6e96b54
dlgksqls <horainlee87@gmail.com>
2024-12-28 18:51:29 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -1,5 +1,8 @@
 package io.secyruty.springsecuritymaster;
-
+import org.springframework.security.core.Authentication;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.annotation.CurrentSecurityContext;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +12,29 @@ public class IndexController {
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/loginPage")
+    public String login(){
+        return "loginPage";
+    }
+
+    @GetMapping("/anonymous")
+    public String anonymous(){
+        return "anonymous";
+    }
+
+    @GetMapping("/authentication")
+    public String authentication(Authentication authentication){
+
+        if (authentication instanceof AnonymousAuthenticationToken) {
+            return "anonymous";
+        } else {
+            return "null";
+        }
+    }
+    @GetMapping("/anonymousContext")
+    public String anonymousContext(@CurrentSecurityContext SecurityContext context){
+        return context.getAuthentication().getName();
     }
 }
