0e232c882cd893a77056e50f019f364866b72a5d
dlgksqls <horainlee87@gmail.com>
2024-12-30 15:41:07 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -11,37 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "index";
-    }
-
-    @GetMapping("/home")
-    public String home(){
-        return "home";
-    }
-
-    @GetMapping("/loginPage")
-    public String login(){
-        return "loginPage";
-    }
-
-    @GetMapping("/anonymous")
-    public String anonymous(){
-        return "anonymous";
-    }
-
-    @GetMapping("/authentication")
-    public String authentication(Authentication authentication){ // 인증 받았다면 authentication에는 인증 받은 사용자 정보가 들어옴
-        // 인증을 받지 못한다면 null이 들어옴
-        if (authentication instanceof AnonymousAuthenticationToken){
-            return "anonymous";
-        }
-        else{
-            return "null";
-        }
-    }
-
-    @GetMapping("/anonymousContext")
-    public String anonymousContext(@CurrentSecurityContext SecurityContext context){
-        return context.getAuthentication().getName();
+        return "AuthenticationManager 학습";
     }
 }
