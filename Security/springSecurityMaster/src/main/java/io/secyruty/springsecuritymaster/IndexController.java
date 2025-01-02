12e508fa52e992c90e1add3d9c291911aab392fe
dlgksqls <horainlee87@gmail.com>
2025-01-02 16:48:23 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "UserDetails 학습";
+        return "SecurityContextRepository_SecurityContextHolderFilter 학습";
     }
 }
