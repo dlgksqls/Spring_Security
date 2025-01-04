f54222677448edfe1f3eb66135335f721fa0e4db
dlgksqls <horainlee87@gmail.com>
2025-01-04 09:14:41 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -1,5 +1,6 @@
 package io.secyruty.springsecuritymaster;
 
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,7 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public Authentication index(Authentication authentication){
-        return authentication;
+        throw new AuthenticationServiceException("error"); // 인증 예외
+//        return authentication;
     }
 
     @GetMapping("/home")
@@ -16,9 +18,19 @@ public class IndexController {
         return "home";
     }
 
-    @GetMapping("/loginPage")
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/login")
     public String login(){
-        return "loginPage";
+        return "LoginPage";
+    }
+
+    @GetMapping("/denied")
+    public String denied(){
+        return "denied";
     }
 
 }
