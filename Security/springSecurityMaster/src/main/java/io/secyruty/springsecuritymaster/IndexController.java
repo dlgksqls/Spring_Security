7ec7a80cfa3df31443525cb039704858a4569c80
dlgksqls <horainlee87@gmail.com>
2025-01-07 13:48:54 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -1,8 +1,13 @@
 package io.secyruty.springsecuritymaster;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.http.HttpRequest;
 
 @RestController
 public class IndexController {
@@ -21,4 +26,27 @@ public class IndexController {
         return "loginPage";
     }
 
+    @PostMapping("/csrf")
+    public String csrf() {
+        return "csrf 적용됨";
+    }
+
+    @GetMapping("/csrfToken")
+    public String csrfToken(HttpServletRequest request){
+        CsrfToken csrfToken1 = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+        CsrfToken csrfToken2 = (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+
+        String token = csrfToken1.getToken();
+        return token;
+    }
+
+    @PostMapping("/formCsrf")
+    public CsrfToken formCsrf(CsrfToken csrfToken) {
+        return csrfToken;
+    }
+
+    @PostMapping("/cookieCsrf")
+    public CsrfToken cookieCsrf(CsrfToken csrfToken) {
+        return csrfToken;
+    }
 }
--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -10,6 +10,9 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.XorCsrfTokenRequestAttributeHandler;
 
 @EnableWebSecurity
 @Configuration
@@ -18,15 +21,41 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
+        SpaCsrfTokenRequestHandler csrfTokenRequestHandler = new SpaCsrfTokenRequestHandler();
+
         http
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/csrf", "/csrfToken", "/form", "/formCsrf", "/cookieCsrf", "/cookie").permitAll()
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                        .csrfTokenRequestHandler(csrfTokenRequestHandler)
+                )
+                .addFilterBefore(new CsrfCookieFilter(), BasicAuthenticationFilter.class) // CsrfCookieFilter를 BasicAuthenticationFilter전에 추가하겠다
                 ;
 
         return http.build();
     }
+
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        CookieCsrfTokenRepository csrfTokenRepository = new CookieCsrfTokenRepository(); // default 는 세션에 csrf 를 저장함
+//        XorCsrfTokenRequestAttributeHandler csrfTokenRequestAttributeHandler = new XorCsrfTokenRequestAttributeHandler();
+//        csrfTokenRequestAttributeHandler.setCsrfRequestAttributeName(null); // null 로 하면 즉시로딩, default 는 지연 로딩(이걸 추천)
+//
+//        http
+//                .authorizeHttpRequests( auth -> auth
+//                        .requestMatchers("/csrf", "/csrfToken", "/form", "/formCsrf").permitAll()
+//                        .anyRequest().authenticated())
+//                .formLogin(Customizer.withDefaults())
+//                .csrf(Customizer.withDefaults())
+////                .csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+////                        .csrfTokenRequestHandler(csrfTokenRequestAttributeHandler))
+//                ;
+//
+//        return http.build();
+//    }
 
     @Bean
     public UserDetailsService userDetailsService(){
