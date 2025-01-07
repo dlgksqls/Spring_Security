650087ab88a7eaa5f0910cff93658d15b9cb8101
dlgksqls <horainlee87@gmail.com>
2025-01-08 08:58:53 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -2,6 +2,7 @@ package io.secyruty.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,19 +11,34 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain1(HttpSecurity http, HandlerMappingIntrospector interceptor) throws Exception {
 
         http
                 .authorizeHttpRequests( auth -> auth
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 ;
+
+        return http.build();
+    }
+
+    @Bean
+    @Order(1) // 이 요청을 젤 우선시 하겠다.
+    public SecurityFilterChain securityFilterChain2(HttpSecurity http, HandlerMappingIntrospector interceptor) throws Exception {
+
+        http
+                .securityMatchers(matchers -> matchers.requestMatchers("/api/**", "/oauth/**"))
+                .authorizeHttpRequests( auth -> auth.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+        ;
 
         return http.build();
     }
