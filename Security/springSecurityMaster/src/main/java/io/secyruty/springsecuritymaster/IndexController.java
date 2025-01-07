c6af459daa19f2c3d585aee401f9dfdf3b9a5022
dlgksqls <horainlee87@gmail.com>
2025-01-08 08:09:22 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -2,23 +2,49 @@ package io.secyruty.springsecuritymaster;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
-    @GetMapping("/")
-    public Authentication index(Authentication authentication){
-        return authentication;
+    @GetMapping("/user")
+    public String user(){
+        return "user";
     }
 
-    @GetMapping("/home")
-    public String home(){
-        return "home";
+    @GetMapping("/myPage/points")
+    public String myPage(){
+        return "myPage";
     }
 
-    @GetMapping("/loginPage")
-    public String login(){
-        return "loginPage";
+    @GetMapping("/manager")
+    public String manager(){
+        return "manager";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/admin/payment")
+    public String adminPayment(){
+        return "adminPayment";
+    }
+
+    @GetMapping("/resource/address_01")
+    public String address_01(){
+        return "address_01";
+    }
+
+    @GetMapping("/resource/address01")
+    public String address01(){
+        return "address01";
+    }
+
+    @PostMapping("/post")
+    public String post(){
+        return "post";
     }
 
 }
