1f4497ae9203d83a0e59d42bc1feaaa97b585493
dlgksqls <horainlee87@gmail.com>
2025-01-08 12:46:34 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/Account.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/Account.java
@@ -1,10 +1,13 @@
 package io.secyruty.springsecuritymaster;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Getter
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Account {
 
     private String owner;
--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -21,9 +22,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http
-                .authorizeHttpRequests( auth -> auth
+                .authorizeHttpRequests(authorize -> authorize
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 ;
 
         return http.build();
