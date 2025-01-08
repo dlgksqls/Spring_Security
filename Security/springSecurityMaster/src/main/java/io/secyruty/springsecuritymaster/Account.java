c66a214de7bcc3546367855816bfcc00ceab8ffb
dlgksqls <horainlee87@gmail.com>
2025-01-08 09:21:00 +0900

    study

---
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/Account.java
@@ -0,0 +1,12 @@
+package io.secyruty.springsecuritymaster;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Account {
+
+    private String owner;
+    private boolean isSecure;
+}
