32b4157b52fdeda469edb1dc502ea0b8060a073f
dlgksqls <horainlee87@gmail.com>
2025-01-09 11:48:18 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -20,16 +22,25 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests( auth -> auth
+                        .requestMatchers("/user").hasRole("USER")
+                        .requestMatchers("/db").hasRole("DB")
+                        .requestMatchers("admin").hasRole("ADMIN")
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
                 ;
 
         return http.build();
     }
 
     @Bean
+    public GrantedAuthorityDefaults grantedAuthorityDefaults(){ // ex) user 경로에 접근하기 위해서는 MYPREFIX_가 필요하다,, role : MYPREFIX_USER
+        return new GrantedAuthorityDefaults("MYPREFIX_");
+    }
+
+    @Bean
     public UserDetailsService userDetailsService(){
-        UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails user = User.withUsername("user").password("{noop}1111").authorities("MYPREFIX_USER").build();
         UserDetails db = User.withUsername("db").password("{noop}1111").roles("DB").build();
         UserDetails admin = User.withUsername("admin").password("{noop}1111").roles("ADMIN","SECURE").build();
         return  new InMemoryUserDetailsManager(user, db, admin);
