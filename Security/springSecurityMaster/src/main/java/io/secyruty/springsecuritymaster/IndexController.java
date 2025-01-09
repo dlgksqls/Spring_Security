176aa4bab60b3c306b92bfb024a2a54a6fea3fda
dlgksqls <horainlee87@gmail.com>
2025-01-09 13:56:55 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/IndexController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index(){
-        return "RoleHierarchy 학습";
+        return "RequestMatcherDelegatingAuthorizationManager 응용 학습";
     }
 }
