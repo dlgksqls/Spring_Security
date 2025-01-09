8047af16730ea36e497a86331237d6f6385757ca
dlgksqls <horainlee87@gmail.com>
2025-01-09 13:51:39 +0900

    study

--- Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
+++ Security/springSecurityMaster/src/main/java/io/secyruty/springsecuritymaster/SecurityConfig.java
@@ -2,6 +2,9 @@ package io.secyruty.springsecuritymaster;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authorization.AuthenticatedAuthorizationManager;
+import org.springframework.security.authorization.AuthorityAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -11,7 +14,14 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+import org.springframework.security.web.access.intercept.RequestAuthorizationContext;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcherEntry;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @EnableWebSecurity
 @Configuration
@@ -22,10 +32,6 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests( auth -> auth
-                        .requestMatchers("/user").hasRole("USER")
-                        .requestMatchers("/db").access(new WebExpressionAuthorizationManager("hasRole('DB')"))
-                        .requestMatchers("/admin").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/secure").access(new CustomAuthorizationManager())
                         .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
@@ -33,6 +39,35 @@ public class SecurityConfig {
 
         return http.build();
     }
+
+    @Bean
+    public AuthorizationManager<RequestAuthorizationContext> authorizationManager(HandlerMappingIntrospector introspector){
+        List<RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>>> mappings = new ArrayList<>();
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry1
+                = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/user"),
+                AuthorityAuthorizationManager.hasAuthority("ROLE_USER"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry2
+                = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/db"),
+                AuthorityAuthorizationManager.hasAuthority("ROLE_DB"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry3
+                = new RequestMatcherEntry<>(new MvcRequestMatcher(introspector, "/admin"),
+                AuthorityAuthorizationManager.hasAuthority("ROLE_ADMIN"));
+
+        RequestMatcherEntry<AuthorizationManager<RequestAuthorizationContext>> requestMatcherEntry4
+                = new RequestMatcherEntry<>(AnyRequestMatcher.INSTANCE, new AuthenticatedAuthorizationManager<>());
+
+        mappings.add(requestMatcherEntry1);
+        mappings.add(requestMatcherEntry2);
+        mappings.add(requestMatcherEntry3);
+        mappings.add(requestMatcherEntry4);
+
+        return new CustomRequestMatcherDelegatingAuthorizationManager(mappings);
+
+    }
+
     @Bean
     public UserDetailsService userDetailsService(){
         UserDetails user = User.withUsername("user").password("{noop}1111").roles("USER").build();
