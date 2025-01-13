18e35b28f3df29e97f411a61453102c80d7880f5
dlgksqls <horainlee87@gmail.com>
2025-01-13 19:15:27 +0900

    study

--- Security/springSecurityMaster/src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java
+++ Security/springSecurityMaster/src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java
@@ -1,4 +1,4 @@
-package io.secyruty.springsecuritymaster;
+package io.security.springsecuritymaster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
